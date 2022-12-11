140f564d1ef701fe5aacc7d5aeab05ea3ec7965a
kimujae <dnwo0103@naver.com>
2022-12-12 06:22:22 +0900

    준최종

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'XE',
+        'USER': 'USER1',
+        'PASSWORD':'1234',
+        'HOST' : 'DESKTOP-4G64VU8',
+        'PORT' : '1521',
     }
 }
 
